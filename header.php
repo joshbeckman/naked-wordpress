5521cb36986f1ec301312dba583e8ed3f014a3fa
Josh <jsh@bckmn.com>
2015-09-22 09:22:30 -0500

    Let's be explicit about an html tag
    Y'know, for the crappy browsers!

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	/*-----------------------------------------------------------------------------------*/
 ?>
 <!DOCTYPE html <?php language_attributes(); ?>>
+<html>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
